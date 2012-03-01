138efb812e617056784e4f4f511dd9fc84e3ee05
Naaman Newbold <naaman@heroku.com>
2012-02-29 16:34:31 -0800

    barebones

---
+++ src/main/java/Main.java
@@ -0,0 +1,27 @@
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+/**
+ * TODO: Javadoc
+ *
+ * @author Naaman Newbold
+ */
+public class Main {
+
+    public static void main(String... args) throws Exception {
+        String providedPort = System.getProperty("port", System.getenv("PORT"));
+        int port = Integer.valueOf(providedPort != null ? providedPort : "8080");
+
+        Server server = new Server(port);
+
+        ServletContextHandler context = new ServletContextHandler(server, "/");
+        context.addServlet(RootServlet.class, "/");
+
+        server.start();
+        server.join();
+    }
+
+}
